700b1c91f4d1a2b86b20d1e7b79e108e83b8c14c
Simon Wilson <contact@simonwilson.design>
2022-02-10 11:18:04 +0000

    first commit

---
+++ web/gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }]
+};
