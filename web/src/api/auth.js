dd0f2937313b010fc69280ad3928d4842ed1064e
Simon Wilson <contact@simonwilson.design>
2022-04-20 20:27:50 +0100

    Removed tls

--- web/src/api/auth.js
+++ web/src/api/auth.js
@@ -4,7 +4,6 @@ const pusher = new Pusher({
     key: "f7ee7537880eb58daa4a",
     secret: "6eda52651a5958b8b5fe",
     cluster: "eu",
-    useTLS: true,
 });
 
 export default async function handler(req, res) {
